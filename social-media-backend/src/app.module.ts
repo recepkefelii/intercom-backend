c2024e4f44062b07c793a4730438a0b2c4a02a39
rewaco <recepkefeliii@gmail.com>
2022-11-03 13:15:18 +0300

    feat: typeorm configuration added

--- social-media-backend/src/app.module.ts
+++ social-media-backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { typeOrmConfig } from './config/typeorm.config';
+
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule,ConfigModule.forRoot({isGlobal:true}),
+  TypeOrmModule.forRootAsync(typeOrmConfig)
+  ],
+  
   controllers: [],
   providers: [],
 })
