df757281863ef53c360a0b967b4f94164f3d11c0
rewaco <recepkefeliii@gmail.com>
2022-11-05 21:02:33 +0300

    delete: typeorm

--- social-media-backend/src/follow/follow.service.ts
+++ social-media-backend/src/follow/follow.service.ts
@@ -1,9 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { from, map, Observable, of, switchMap } from 'rxjs';
 import { UserFolllowDto } from './dto';
 import { FallowEntity } from 'src/entities/follow.entity';
-import { Repository } from 'typeorm/repository/Repository';
 import { FollowRequest } from './interface/follow.interface';
 import { UserEntity } from 'src/entities';
 
