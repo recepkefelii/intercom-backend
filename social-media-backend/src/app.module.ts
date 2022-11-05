ef840d67d32ef028a47a85692feaba271d75513d
rewaco <recepkefeliii@gmail.com>
2022-11-05 21:14:35 +0300

    feat: added prisma init

--- social-media-backend/src/app.module.ts
+++ social-media-backend/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { FollowModule } from './follow/follow.module';
 
@@ -10,7 +8,6 @@ import { FollowModule } from './follow/follow.module';
 @Module({
   imports: [AuthModule,
   ConfigModule.forRoot({ isGlobal:true }),
-  TypeOrmModule.forRootAsync(typeOrmConfig),
   UserModule,
   FollowModule 
   ],
