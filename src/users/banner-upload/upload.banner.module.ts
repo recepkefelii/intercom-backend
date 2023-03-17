a69ad0e6aa06cf8dfaeee3a978e00604036fd37d
recepkefelii <recepkefeliii@gmail.com>
2023-03-17 21:25:34 +0300

    add: banner service update aws s3

--- src/users/banner-upload/upload.banner.module.ts
+++ src/users/banner-upload/upload.banner.module.ts
@@ -2,14 +2,18 @@ import { Module } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "src/auth/schema/user.schema";
+import { AwsS3Module } from "src/aws-s3/aws-s3.module";
 import { UploadBannerController } from "./upload.banner.controller";
 import { UploadBannerService } from "./upload.banner.service";
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+    imports: [
+        MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+        AwsS3Module
+    ],
     controllers: [UploadBannerController],
     providers: [JwtService, UploadBannerService],
-    exports : [UploadBannerService]
+    exports: [UploadBannerService]
 
 })
 export class UploadBannerModule { }
--- src/users/banner-upload/upload.banner.service.ts
+++ src/users/banner-upload/upload.banner.service.ts
@@ -1,31 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { extname } from "path";
 import { User, UserDocument } from "src/auth/schema/user.schema";
 import { UserdDto } from "../dto/user.dto";
-import sharp from 'sharp';
+import { AwsS3Service } from "src/aws-s3/aws-s3.service";
 
 @Injectable()
 export class UploadBannerService {
-    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
+    constructor(
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
+        private readonly awsS3Service: AwsS3Service
+    ) { }
 
     async uploadBanner(file: Express.Multer.File, user: UserdDto) {
         const userProfile = await this.userModel.findById(user.id).select("-password");
-
-        const buffer = await sharp(file.buffer)
-            .resize(1178, 400)
-            .toBuffer();
-
-        const uniqueSuffix =
-            Date.now() + '-' + Math.round(Math.random() * 1e9);
-        const ext = extname(file.originalname);
-        const filename = `${uniqueSuffix}${ext}`;
-
-        userProfile.banner = filename;
-
-        await sharp(buffer).toFile(`./upload/${filename}`);
-
+        const uploadedFile = await this.awsS3Service.uploadFile(file.buffer, file.originalname);
+        userProfile.banner_url = uploadedFile.url
         return await userProfile.save();
     }
 }
