37d596d67c79ee1320e9918251ed8d8c7dbb1e8e
recepkefelii <recepkefeliii@gmail.com>
2023-03-19 14:20:11 +0300

    fix: create post service

--- src/post/create-post/create.post.module.ts
+++ src/post/create-post/create.post.module.ts
@@ -1,6 +1,7 @@
 import { Module } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "src/auth/schema/user.schema";
 import { AwsS3Module } from "src/aws-s3/aws-s3.module";
 import { Post, PostSchema } from "../schemas/post.schema";
 import { CreatePostController } from "./create.post.controller";
@@ -8,7 +9,7 @@ import { CreatePostService } from "./create.post.service";
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }]),
+        MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }, { name: User.name, schema: UserSchema }]),
         AwsS3Module
     ],
     providers: [CreatePostService, JwtService],
--- src/post/create-post/create.post.service.ts
+++ src/post/create-post/create.post.service.ts
@@ -5,28 +5,35 @@ import { Model } from "mongoose";
 import { UserdDto } from "src/users/dto/user.dto";
 import { CreatePostDto } from "./dto/create.post.dto";
 import { AwsS3Service } from "src/aws-s3/aws-s3.service";
+import { User, UserDocument } from "src/auth/schema/user.schema";
 
 @Injectable()
 export class CreatePostService {
     constructor(
         @InjectModel(Post.name) private postModel: Model<PostDocument>,
+        @InjectModel(User.name) private userModel: Model<UserDocument>,
         private readonly awsS3Service: AwsS3Service
     ) { }
 
     async createPost(file: Express.Multer.File, post: CreatePostDto, author: UserdDto): Promise<Post> {
+        // const user = await this.userModel.findById(author.id)
         let photo_url = null
         if (file) {
             const uploadedFile = await this.awsS3Service.uploadFile(file.buffer, file.originalname);
             photo_url = uploadedFile.url;
         }
-    
+
         const createPost = await this.postModel.create({
             content: post.content,
             title: post.title,
-            author: author,
+            author: author.id,
             photo_url: photo_url
         });
+        const user = await this.userModel.findById(author.id);
+        user.postsCount++
+        user.posts.push(createPost._id);
+        await user.save();
         return createPost;
     }
-    
+
 }
